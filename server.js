5a8fc5f151a3be8a6acca379407aa001ef5fdb10
Sergio Camilo <sergiocamiloe@gmail.com>
2022-11-07 17:42:22 -0600

    ADD - express library and start server

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 'use strict'
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((request, response) => {
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/html');
-    response.end('<h1>Server is working</h1>');
-});
+const PORT = 3000;
+const HOSTNAME = 'localhost';
 
-server.listen(port, hostname, () => {
-    console.log(`Server is running on http://${hostname}:${port}/`);
-});
+var app = express();
+
+app.listen(PORT, HOSTNAME, () => {
+    console.log(`Server is running on http://${HOSTNAME}:${PORT}/`);
+})
